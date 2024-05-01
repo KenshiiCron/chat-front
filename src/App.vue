0294419c9b7749259b5aa99311e844ff1b0eadcf
Skandar <skandarcron@gmail.com>
2024-05-01 10:39:43 +0000

    hehe

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,10 @@ import AssistantMsg from './components/AssistantMsg.vue';
 const msgInput = ref()
 const input = ref(null)
 const texts = reactive([{ type: 'assistant', content: 'hey' }, { type: 'client', content: 'hello' }])
-const addClientMsg = () => {
+const addClientMsg = (e) => {
   texts.push({ type: 'client', content: msgInput.value })
+  msgInput.value = ''
+  e.preventDefault();
 }
 
 </script>
@@ -20,13 +22,13 @@ const addClientMsg = () => {
     </div>
     <div class="flex-1 overflow-y-scroll message-wrapper">
       <div v-for='(text, index) in texts' :key="index">
-        <ClientMsg v-if="text.type == 'client'" :text='text.content' />
+        <ClientMsg v-if="text.type == 'assistant'" :text='text.content' />
         <AssistantMsg v-else :text='text.content' />
       </div>
     </div>
-    <div class='w-full  border-sky-400 border-solid border-2 rounded-md text-black flex'>
+    <form @submit = 'addClientMsg' class='w-full  border-sky-400 border-solid border-2 rounded-md text-black flex'>
       <input type="text" class='p-1 w-full' v-model='msgInput' ref='input' id="inp">
-      <button @click='addClientMsg'>send</button>
-    </div>
+      <button type = 'submit'>send</button>
+    </form>
   </main>
 </template>
--- src/components/AssistantMsg.vue
+++ src/components/AssistantMsg.vue
@@ -5,7 +5,7 @@
 <template lang="">
     <div class='p-1.5 w-full flex justify-end '>
         <div class = 'w-7/12 max-w-fit' >
-            <h3 class = 'text-sky-400'>You</h3>
+            <h3 class = 'text-sky-400'>Assistant</h3>
             <div class = 'bg-sky-700 p-1 '>
                 <p>hey
                 </p>            
